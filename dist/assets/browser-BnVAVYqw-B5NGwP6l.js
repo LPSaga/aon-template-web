fa8f689c6794ad32bdc822332b4cb8b2905cf2f0
owen <lpsaga10@163.com>
2024-12-28 12:45:25 +0800

    feat: build

---
+++ dist/assets/browser-BnVAVYqw-B5NGwP6l.js
@@ -0,0 +1,1 @@
+import{L as e}from"./index-B5IPw5aU.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const t=e(o),n=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"}));export{n as b};
